bb951406de0577a28a26529756eac4432048cfd6
[luvoul] <85976276+luvoul@users.noreply.github.com>
2021-09-25 09:28:53 +0900

    update

---
+++ 5.10~5.14/7. Jiyeon/gl.js
@@ -0,0 +1,7 @@
+<%@page import = "dto.BbsDto"%>
+<%@page import = "java.util.List"%>
+<%@page import = "dao.BbsDao"%>
+<%@page import = "dto.MemberDto"%>
+<%@ page language = "java" contentType="text/html; charsetUTF=8"
+    pageEncoding = "UTF-8"%>
+
